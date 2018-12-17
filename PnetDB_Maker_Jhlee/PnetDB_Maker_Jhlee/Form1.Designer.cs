53dae56716df849b414d79dec286f89a00f176e6
juhyup12 <juhyup12@gmail.com>
2018-12-17 13:16:49 +0900

    PnetDB_Maker_Jhlee Version Start ( Read_InputFiled add)

---
+++ PnetDB_Maker_Jhlee/PnetDB_Maker_Jhlee/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace PnetDB_Maker_Jhlee
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.AllowDrop = true;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(80, 95);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(294, 16);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.DragDrop += new System.Windows.Forms.DragEventHandler(this.listView1_DragDrop);
+            this.listBox1.DragEnter += new System.Windows.Forms.DragEventHandler(this.listView1_DragEnter);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(91, 252);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(128, 70);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Run";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.button1_MouseClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ PnetDB_Maker_Jhlee/PnetDB_Maker_Jhlee/Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using Excel = Microsoft.Office.Interop.Excel;                                           //COM for excel file handling
+
+namespace PnetDB_Maker_Jhlee
+{
+    public partial class Form1 : Form
+    {
+
+        bool isFullListBox = false;// 한번 ListBox채우면 더 못채우도록
+        string Path=null;
+        string InputFile_Path=null;
+
+        object[,] ReadData;
+
+        Excel.Application Excel_App = null;
+        Excel.Workbook Rd_Wb = null;
+        Excel.Worksheet Rd_Ws = null;
+
+        Excel.Workbook Wr_Wb = null;
+        Excel.Worksheet Wr_Ws = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void listView1_DragDrop(object sender, DragEventArgs e)// 이벤트 함수는 리스트에 파일의 제목을 추가하는 내용임
+        {
+            if(false ==isFullListBox)
+            {
+                string[] Files = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+                foreach (string File in Files)
+                {
+                    listBox1.Items.Add(File);
+                    InputFile_Path += File;
+
+                }
+
+                
+                isFullListBox = true;
+
+            }
+        }
+
+        private void listView1_DragEnter(object sender, DragEventArgs e)// 이거 안되어있으면, 파일이 리스트에 추가되지않는것처럼 보임 
+        {
+            e.Effect = DragDropEffects.Copy;
+
+        }
+
+        private void button1_MouseClick(object sender, MouseEventArgs e)// RunButton Inputfile이 없으면 아무것도 실행안된다.
+        {
+            if (InputFile_Path == null)
+                return;
+
+            Read_InputFile();
+
+            //버튼1을 누르면 Excel에 데이터를 읽어들여서(Read_InputFile()-> PnetCompoint.xlxs 형태로 만든다
+
+
+        }
+
+        private void Read_InputFile()
+        {
+
+            try
+            {
+                Excel_App = new Excel.Application();
+                Rd_Wb = Excel_App.Workbooks.Open("@" + InputFile_Path);// 해당 경로에 워크북 열기
+                Rd_Ws = Rd_Wb.Worksheets.Add();//Work Sheet추가 
+
+                Excel.Range rng = Rd_Ws.UsedRange;
+                ReadData = rng.Value;
+
+
+            }
+
+            finally
+            {
+                ReleaseExcelObject(Rd_Ws);
+                ReleaseExcelObject(Rd_Wb);
+                ReleaseExcelObject(Excel_App);
+            }
+
+
+
+
+
+        }
+        private static void ReleaseExcelObject(object obj)
+        {
+            try
+            {
+
+                if (obj != null)
+                {
+                    Marshal.ReleaseComObject(obj);
+                    obj = null;
+
+                }
+            }
+
+            catch (Exception ex)
+            {
+                obj = null;
+                throw ex;
+
+            }
+            finally
+            {
+                GC.Collect();
+
+            }
+
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            Path = System.IO.Directory.GetCurrentDirectory();// Form이 열리자마자, 폴더 실행경로 받아오기
+
+
+
+        }
+    }
+}
